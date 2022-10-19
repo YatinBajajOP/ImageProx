5f4e0633420d4774c2423a87460a5f7f5be571c3
YatinBajajOP <yatin19csu360@ncuindia.edu>
2022-10-20 02:17:57 +0530

    to see some progress

--- src/functions.py
+++ src/functions.py
@@ -39,7 +39,7 @@ def powerTransform(path, c, gamma):
         c = 255 / np.log(1 + np.max(img))
     log_image = np.array(c*(img/c)**gamma,dtype='uint8')
     log_image = np.array(log_image, dtype = np.uint8)
-    output_path = "..//static//output//power//" + path.split("//")[-1]
+    output_path = ".//static//output//power//" + path.split("//")[-1]
     cv2.imwrite(output_path, log_image)
     st.image(output_path,  caption="Powered Image")
     btn = st.download_button(
@@ -54,7 +54,7 @@ def rotate(path, degree):
     img = cv2.imread(path)
     M = cv2.getRotationMatrix2D((img.shape[1]/2, img.shape[0]/2), degree, 1) 
     rotated_img = cv2.warpAffine(img, M, (img.shape[1], img.shape[0])) 
-    output_path = "..//static//output//rotate//" + path.split("//")[-1]
+    output_path = ".//static//output//rotate//" + path.split("//")[-1]
     cv2.imwrite(output_path, rotated_img)
     st.image(output_path,  caption="Rotated Image")
     btn = st.download_button(
@@ -78,7 +78,7 @@ def add_saltnpepper(path, x, y):
         y_coord=random.randint(0, row - 1)
         x_coord=random.randint(0, col - 1)
         img[y_coord][x_coord] = 0
-    output_path = "..//static//output//saltnpepper//" + path.split("//")[-1]
+    output_path = ".//static//output//saltnpepper//" + path.split("//")[-1]
     cv2.imwrite(output_path, img)
     st.image(output_path,  caption="Noisy Image")
     btn = st.download_button(
@@ -94,7 +94,7 @@ def add_boxes(path, num_boxes, size_x, size_y):
         x, y = np.random.randint(0,max(img.shape[0], img.shape[1]) - max(box_x,box_y), size=2)
         img[x:x + box_x, y:y+box_y, :] = 255
     
-    output_path = "..//static//output//boxes//" + path.split("//")[-1]
+    output_path = ".//static//output//boxes//" + path.split("//")[-1]
     cv2.imwrite(output_path, img)
     st.image(output_path,  caption="Boxy Image")
     btn = st.download_button(
@@ -116,7 +116,7 @@ def contrastStretching(path, r1, s1, r2, s2):
     img = cv2.imread(path)
     pixelVal_vec = np.vectorize(pixelVal)
     contrast_stretched = pixelVal_vec(img, r1, s1, r2, s2)
-    output_path = "..//static//output//contrast//" + path.split("//")[-1]
+    output_path = ".//static//output//contrast//" + path.split("//")[-1]
     cv2.imwrite(output_path, contrast_stretched)
     st.image(output_path,  caption="Contrasted Image")
     btn = st.download_button(
