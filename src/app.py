04a6a29be6db32a29d111c39bc1c95b85272e9a1
YatinBajajOP <yatin19csu360@ncuindia.edu>
2022-10-27 15:18:16 +0530

    resolved errors

--- src/app.py
+++ src/app.py
@@ -52,11 +52,11 @@ log_image = np.array(log_image, dtype = np.uint8)
         ''', language='python')
         img = st.file_uploader(label = "Upload the image") 
         # st.radio("Value of c", ) 
-        path = storeImage(img, "log")
         c = -1
         if not st.checkbox("Default value of c (Scaling factor)"):
             c = st.slider("Value of c:", min_value=0, max_value=255, value=1)
         if img is not None:
+            path = storeImage(img, "log")
             logTransform(path, c)
 
     elif transformation == "Power":
@@ -87,12 +87,12 @@ gamma_corrected = np.array(255*(img / 255) ** gamma, dtype = 'uint8')
 cv2.imwrite('gamma_transformed'+str(gamma)+'.jpg', gamma_corrected''',language="python")
         img = st.file_uploader(label = "Upload the image") 
         # st.radio("Value of c", ) 
-        path = storeImage(img, "power")
         c = -1
         if not st.checkbox("Default value of c (Scaling factor)"):
             c = st.slider("Value of c:", min_value=0, max_value=255, value=255)
         gamma = st.slider("Value of Gamma:", min_value=0.0, max_value=8.0, value=0.4)
         if img is not None:
+            path = storeImage(img, "power")
             powerTransform(path, c, gamma)
 
 
@@ -101,12 +101,12 @@ elif choice == "Spatial Filtering":
     filter = st.sidebar.radio("Spatial Filtering", filters, key="Filter")
     if filter == "Contrast":
         img = st.file_uploader(label = "Upload the image") 
-        path = storeImage(img, "contrast")
         r1 = st.slider("Choose the value of r1 :", min_value=0, max_value=255, value=70)
         s1 = st.slider("Choose the value of s1 :", min_value=0, max_value=255, value=0)
         r2 = st.slider("Choose the value of r2 :", min_value=0, max_value=255, value=140)
         s2 = st.slider("Choose the value of s2 :", min_value=0, max_value=255, value=255)
         if img is not None:
+            path = storeImage(img, "contrast")
             contrastStretching(path, r1, s1, r2, s2)
     
 
@@ -116,9 +116,9 @@ elif choice == "Custom Editing":
     edit = st.sidebar.radio("Custom Editing", edits, key="Edit")
     if edit == "Rotate":
         img = st.file_uploader(label = "Upload the image")
-        path = storeImage(img, "rotate")
         degree = st.slider("Select the rotation angle", min_value=-180, max_value=180, value = 90)
         if img is not None:
+            path = storeImage(img, "rotate")
             rotate(path, degree)
         
 elif choice == "Other Functionalities":
@@ -130,8 +130,6 @@ elif choice == "Adding noise":
     noise = st.sidebar.radio("Noise addition", noises, key="Noise")
     if noise == "Salt and pepper noise":
         st.header("Salt and pepper noise")
-
-
         st.text(''' It is found only in grayscale images (black and white image). 
 As the name suggests salt (white) in pepper (black)-white spots in
 the dark regions or pepper (black) in salt (white)-black spots in
@@ -145,66 +143,65 @@ analog-to-digital conversion error, bit transmission error, etc.''')
         st.write("Refer to the code below:")
 
         st.code('''import random
-        import cv2
+import cv2
 
-        def add_noise(img):
+def add_noise(img):
 
-            # Getting the dimensions of the image
-            row , col = img.shape
-            
-            # Randomly pick some pixels in the
-            # image for coloring them white
-            # Pick a random number between 300 and 10000
-            number_of_pixels = random.randint(300, 10000)
-            for i in range(number_of_pixels):
-            
-                # Pick a random y coordinate
-                y_coord=random.randint(0, row - 1)
-                
-                # Pick a random x coordinate
-                x_coord=random.randint(0, col - 1)
-                
-                # Color that pixel to white
-                img[y_coord][x_coord] = 255
-                
-            # Randomly pick some pixels in
-            # the image for coloring them black
-            # Pick a random number between 300 and 10000
-            number_of_pixels = random.randint(300 , 10000)
-            for i in range(number_of_pixels):
-            
-                # Pick a random y coordinate
-                y_coord=random.randint(0, row - 1)
-                
-                # Pick a random x coordinate
-                x_coord=random.randint(0, col - 1)
-                
-                # Color that pixel to black
-                img[y_coord][x_coord] = 0
-                
-            return img
+    # Getting the dimensions of the image
+    row , col = img.shape
+    
+    # Randomly pick some pixels in the
+    # image for coloring them white
+    # Pick a random number between 300 and 10000
+    number_of_pixels = random.randint(300, 10000)
+    for i in range(number_of_pixels):
+    
+        # Pick a random y coordinate
+        y_coord=random.randint(0, row - 1)
+        
+        # Pick a random x coordinate
+        x_coord=random.randint(0, col - 1)
+        
+        # Color that pixel to white
+        img[y_coord][x_coord] = 255
+        
+    # Randomly pick some pixels in
+    # the image for coloring them black
+    # Pick a random number between 300 and 10000
+    number_of_pixels = random.randint(300 , 10000)
+    for i in range(number_of_pixels):
+    
+        # Pick a random y coordinate
+        y_coord=random.randint(0, row - 1)
+        
+        # Pick a random x coordinate
+        x_coord=random.randint(0, col - 1)
+        
+        # Color that pixel to black
+        img[y_coord][x_coord] = 0
+        
+    return img
 
-        # salt-and-pepper noise can
-        # be applied only to grayscale images
-        # Reading the color image in grayscale image
-        img = cv2.imread('lena.jpg',
-                        cv2.IMREAD_GRAYSCALE)
+# salt-and-pepper noise can
+# be applied only to grayscale images
+# Reading the color image in grayscale image
+img = cv2.imread('lena.jpg',
+                cv2.IMREAD_GRAYSCALE)
 
-        #Storing the image
-        cv2.imwrite('salt-and-pepper-lena.jpg',
-                    add_noise(img))
+#Storing the image
+cv2.imwrite('salt-and-pepper-lena.jpg',
+            add_noise(img))
         ''',language="python")
         img = st.file_uploader(label = "Upload the image")
-        path = storeImage(img, "saltnpepper")
         x, y = st.slider("Select the range of noise level",
                 300, 10000, (500, 5000)
         )
         if img is not None:
+            path = storeImage(img, "saltnpepper")
             add_saltnpepper(path, x, y)
 
     elif noise == "Squares and rectangles":
         img = st.file_uploader(label = "Upload the image")
-        path = storeImage(img, "boxes")
         x, y = st.slider("Select the range of sizes of boxes",
                 0, 100, (20, 50)
         )
@@ -212,15 +209,10 @@ analog-to-digital conversion error, bit transmission error, etc.''')
                 min_value=0, max_value=50, value = 10
         )
         if img is not None:
+            path = storeImage(img, "boxes")
             add_boxes(path, num_boxes, x, y)
 
 
 else:
     presets = ["Youtube Thumbnail", "Instagram post", "Instagram Story", "Passport size"]
     preset = st.sidebar.radio("App presets", presets, key="Preset")
-
-
-
-    
-
-
