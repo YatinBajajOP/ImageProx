a3c9769e12ff1b8a986db041e7b26f233bd016b2
YatinBajajOP <67017561+YatinBajajOP@users.noreply.github.com>
2022-11-29 11:29:57 +0530

    Add files via upload

--- src/app.py
+++ src/app.py
@@ -3,24 +3,23 @@ from functions import *
 import os
 from datetime import datetime, time
 
+# Streamlit
+st.set_page_config(page_icon="📸", page_title="ImageProx", layout="wide")
+st.sidebar.title("ImageProx 📸")
+st.markdown('<center><h1 style="color:red;">Welcome to ImageProx 📸</h1></center>', unsafe_allow_html=True)
 
-st.title("Welcome to ImageProx")
-choices = ["Transformations", "Spatial Filtering", "Custom Editing", "App presets", "Other Functionalities", "Adding noise"]
+choices = ["Transformations", "Spatial Filtering", "Custom Editing", "Adding noise"]
 choice = st.sidebar.selectbox("Choose the edits:", choices)
 
-
 if choice == "Transformations":
-    transformations = ["Log", "Power", "Cosine", "Fourier", "Haar"]
+    transformations = ["Log", "Power", "Cosine"]
     transformation = st.sidebar.radio("Choose the transformations:", transformations, key="Transform")
     if transformation == "Log":
         st.header("Logarithmic Transformation")
         st.text('''
-        Logarithmic transformation of an image is one of the gray level image 
-transformations. 
-Log transformation of an image means replacing all pixel values, present 
-in the image, with its logarithmic values. 
-Log transformation is used for image enhancement as it expands dark pixels
-of the image as compared to higher pixel values.
+        Logarithmic transformation of an image is one of the gray level image transformations. 
+Log transformation of an image means replacing all pixel values, present in the image, with its logarithmic values. 
+Log transformation is used for image enhancement as it expands dark pixelsof the image as compared to higher pixel values.
 
 The formula for applying log transformation in an image is,''')
         st.code('S = c * log (1 + r)')
@@ -64,11 +63,9 @@ log_image = np.array(log_image, dtype = np.uint8)
 
     elif transformation == "Power":
         st.header("Power Transformation")
-        st.text('''Gamma correction is important for displaying images on a screen correctly, 
-to prevent bleaching or darkening of images when viewed from different 
-types of monitors with different display settings. This is done because 
-our eyes pereive images in a gamma-shaped curve, whereas cameras capture
-images in a linear fashion. Below is the Python code to apply gamma correction.
+        st.text('''Gamma correction is important for displaying images on a screen correctly, to prevent bleaching or darkening of images when viewed from
+different types of monitors with different display settings. This is done because our eyes pereive images in a gamma-shaped curve,
+whereas cameras capture images in a linear fashion. Below is the Python code to apply gamma correction.
 
 ''')
         st.write("Power-law (gamma) transformations can be mathematically expressed as:")
@@ -87,7 +84,7 @@ for gamma in [0.1, 0.5, 1.2, 2.2]:
 gamma_corrected = np.array(255*(img / 255) ** gamma, dtype = 'uint8')
 
 # Save edited images.
-cv2.imwrite('gamma_transformed'+str(gamma)+'.jpg', gamma_corrected''',language="python")
+cv2.imwrite('gamma_transformed'+str(gamma)+'.jpg', gamma_corrected)''',language="python")
         img = st.file_uploader(label = "Upload the image") 
         # st.radio("Value of c", ) 
         c = -1
@@ -98,11 +95,90 @@ cv2.imwrite('gamma_transformed'+str(gamma)+'.jpg', gamma_corrected''',language="
             path = storeImage(img, "power")
             powerTransform(path, c, gamma)
 
+    elif transformation == "Cosine":
+        st.header("Cosine Transformation")
+        st.text(''' Contrast stretching (often called normalization) is a simple image enhancement technique that attempts to improve the contrast in an image 
+by 'stretching' the range of intensity values it contains to span a desired range of values, the full range of pixel values that the image
+type concerned allows.It is a technique that tries to improve the image.
+It improves the contrast in an image by stretching the intensity value range. It is the difference between the maximum and minimum intensity 
+value in an image.''')
+
+        st.write("Refer to the code below:")
+        st.code('''from scipy.fftpack import dct, idct
+
+# implement 2D DCT
+def dct2(a):
+    return dct(dct(a.T, norm='ortho').T, norm='ortho')
+
+# implement 2D IDCT
+def idct2(a):
+    return idct(idct(a.T, norm='ortho').T, norm='ortho')    
+
+from skimage.io import imread
+from skimage.color import rgb2gray
+import numpy as np
+import matplotlib.pylab as plt
+
+# read lena RGB image and convert to grayscale
+im = rgb2gray(imread('images/lena.jpg')) 
+imF = dct2(im)
+im1 = idct2(imF)
+
+# check if the reconstructed image is nearly equal to the original image
+np.allclose(im, im1)
+# True
+
+# plot original and reconstructed images with matplotlib.pylab
+plt.gray()
+plt.subplot(121), plt.imshow(im), plt.axis('off'), plt.title('original image', size=20)
+plt.subplot(122), plt.imshow(im1), plt.axis('off'), plt.title('reconstructed image (DCT IDCT)', size=20)
+plt.show() ''',
+language="python")
+        img = st.file_uploader(label = "Upload the image")
+        if img is not None:
+            path = storeImage(img, "cosine")
+            cosineTransform(path) 
 
 elif choice == "Spatial Filtering":
-    filters = ["Contrast", "Brightness", "Hue", "Smoothning - Mean", "Sharpening", "Median Filter"]
+    filters = ["Contrast", "Smoothning - Mean", "Sharpening", "Median Filter", "Mode Filter"]
     filter = st.sidebar.radio("Spatial Filtering", filters, key="Filter")
     if filter == "Contrast":
+        st.header("Contrast Stretching")
+        st.text(''' Contrast stretching (often called normalization) is a simple image enhancement technique that attempts to improve the contrast in an image by 'stretching' the range of intensity values it contains to span a desired range of values, the full range of pixel values that the image type concerned allows.It is a technique that tries to improve the image.
+It improves the contrast in an image by stretching the intensity value range.It is the difference between the maximum and minimum intensity value in an image.''')
+
+        st.write("Refer to the code below:")
+        st.code('''import cv2
+importnumpy as np
+fromIPython.display import Image
+frommatplotlib import pyplot as plt
+frommatplotlib import pyplot as plt1
+
+frommatplotlib import pyplot as plt2
+
+r1=80
+r2=200
+s1=30
+s2=210
+img = cv2.imread("buoy.jpg",0)
+al=s1/r1
+bt=(s2-s1)/(r2-r1)
+gm=(255-s2)/(255-r2)
+c1=s1-bt*r1
+c2=s2-gm*r2
+d=img
+fori in range(len(img)):
+for j in range(len(img[0])):
+if(img[i][j] < r1):
+d[i][j] = al*img[i][j]
+elif( r1 <= img[i][j] < r2 ):
+d[i][j] = bt*img[i][j]   c1
+else:
+d[i][j] = gm*img[i][j]   c2
+cv2.imwrite('buoy_contrast.jpg',d)
+Image('buoy.jpg')
+Image('buoy_contrast.jpg') ''',
+language="python")
         img = st.file_uploader(label = "Upload the image") 
         r1 = st.slider("Choose the value of r1 :", min_value=0, max_value=255, value=70)
         s1 = st.slider("Choose the value of s1 :", min_value=0, max_value=255, value=0)
@@ -111,40 +187,74 @@ elif choice == "Spatial Filtering":
         if img is not None:
             path = storeImage(img, "contrast")
             contrastStretching(path, r1, s1, r2, s2)
+
+    elif filter == "Sharpening":
+        img = st.file_uploader(label = "Upload the image") 
+        if img is not None:
+            path = storeImage(img, "sharpen")
+            sharpeningImage(path)
+
+    elif filter == "Smoothning - Mean":
+        img = st.file_uploader(label = "Upload the image") 
+        if img is not None:
+            path = storeImage(img, "blur")
+            blurringImage(path)
+
+    elif filter == "Median Filter":
+        img = st.file_uploader(label = "Upload the image") 
+        size = st.slider(label="Select the size of the filter", min_value=1, max_value=15, value=3, step=2)
+        if img is not None:
+            path = storeImage(img, "median")
+            medianFilter(path, size)
+
+    elif filter == "Mode Filter":
+        img = st.file_uploader(label = "Upload the image") 
+        size = st.slider(label="Select the size of the filter", min_value=1, max_value=15, value=3, step=2)
+        if img is not None:
+            path = storeImage(img, "mode")
+            modeFilter(path, size)
     
-
-
 elif choice == "Custom Editing":
-    edits = ["Crop", "Resize", "Rotate", "Shear"]
+    edits = ["Crop", "Resize", "Rotate"]
     edit = st.sidebar.radio("Custom Editing", edits, key="Edit")
+    img = st.file_uploader(label = "Upload the image")
+
     if edit == "Rotate":
-        img = st.file_uploader(label = "Upload the image")
         degree = st.slider("Select the rotation angle", min_value=-180, max_value=180, value = 90)
         if img is not None:
             path = storeImage(img, "rotate")
             rotate(path, degree)
-        
-elif choice == "Other Functionalities":
-    functionalities = ["PDF to Word", "Word to PDF", "JPEG to .txt", "PDF to .txt"]
-    functionality = st.sidebar.radio("Functionalities", functionalities)
+
+    elif edit == "Resize":
+        width = st.number_input("Enter the width : ", min_value=0, max_value=1000, value=50, step=1)
+        height = st.number_input("Enter the height : ", min_value=0, max_value=1000, value=50, step=1)
+        if img is not None:
+            path = storeImage(img, "resize")
+            resize(path, width, height)
+    
+    elif edit == "Crop":
+        x1 = st.number_input("Enter the value of x1", min_value=0, max_value=1000, value=0, step=1)
+        x2 = st.number_input("Enter the value of x2", min_value=0, max_value=1000, value=0, step=1)
+        y1 = st.number_input("Enter the value of y1", min_value=0, max_value=1000, value=50, step=1)
+        y2 = st.number_input("Enter the value of y2", min_value=0, max_value=1000, value=50, step=1)
+        if img is not None:
+            path = storeImage(img, "resize")
+            crop(path, x1, x2, y1, y2)
+
+
 
 elif choice == "Adding noise":
-    noises = ["Salt and pepper noise", "Squares and rectangles", "Gaussian", "Poisson", "Speckle"]
+    noises = ["Salt and pepper noise", "Squares and rectangles", "Gaussian", "Poisson", "Speckle", "Localvar", "Pepper", "Salt", "Salt&Pepper"]
     noise = st.sidebar.radio("Noise addition", noises, key="Noise")
     if noise == "Salt and pepper noise":
         st.header("Salt and pepper noise")
-        st.text(''' It is found only in grayscale images (black and white image). 
-As the name suggests salt (white) in pepper (black)-white spots in
-the dark regions or pepper (black) in salt (white)-black spots in
-the white regions.
-In other words, an image having salt-and-pepper noise will have a
-few dark pixels in bright regions and a few bright pixels in dark 
-regions. Salt-and-pepper noise is also called impulse noise. 
-It can be caused by several reasons like dead pixels, 
+        st.text(''' It is found only in grayscale images (black and white image). As the name suggests salt (white) in pepper (black)-white spots in
+the dark regions or pepper (black) in salt (white)-black spots in the white regions.
+In other words, an image having salt-and-pepper noise will have a few dark pixels in bright regions and a few bright pixels in dark 
+regions. Salt-and-pepper noise is also called impulse noise. It can be caused by several reasons like dead pixels, 
 analog-to-digital conversion error, bit transmission error, etc.''')
 
         st.write("Refer to the code below:")
-
         st.code('''import random
 import cv2
 
@@ -215,10 +325,48 @@ cv2.imwrite('salt-and-pepper-lena.jpg',
             path = storeImage(img, "boxes")
             add_boxes(path, num_boxes, x, y)
 
+    elif noise == "Gaussian":
+        img = st.file_uploader(label = "Upload the image")
+        if img is not None:
+            path = storeImage(img, "gaussian")
+            add_gaussian(path)
+    
+    elif noise == "Poisson":
+        img = st.file_uploader(label = "Upload the image")
+        if img is not None:
+            path = storeImage(img, "poisson")
+            add_poisson(path)
 
-else:
-    presets = ["Youtube Thumbnail", "Instagram post", "Instagram Story", "Passport size"]
-    preset = st.sidebar.radio("App presets", presets, key="Preset")
+    elif noise == "Speckle":
+        img = st.file_uploader(label = "Upload the image")
+        if img is not None:
+            path = storeImage(img, "speckle")
+            add_speckle(path)
+
+    elif noise == "Pepper":
+        img = st.file_uploader(label = "Upload the image")
+        if img is not None:
+            path = storeImage(img, "pepper")
+            add_pepper(path)
+    
+    elif noise == "Salt":
+        img = st.file_uploader(label = "Upload the image")
+        if img is not None:
+            path = storeImage(img, "salt")
+            add_salt(path)
+
+    elif noise == "Salt&Pepper":
+        img = st.file_uploader(label = "Upload the image")
+        if img is not None:
+            path = storeImage(img, "sandp")
+            add_sandp(path)
+
+    elif noise == "Localvar":
+        img = st.file_uploader(label = "Upload the image")
+        if img is not None:
+            path = storeImage(img, "localvar")
+            add_localvar(path)
+
 
 if st.sidebar.button("Clear cache"):
     root = "./static/"
--- src/functions.py
+++ src/functions.py
@@ -1,11 +1,14 @@
 # Functions file
 import cv2
 import streamlit as st
-from PIL import Image
+from PIL import Image, ImageFilter
 import random
 import numpy as np
-from datetime import datetime, time
-
+from datetime import datetime
+import os
+import skimage
+from scipy.fftpack import idct, dct
+from skimage.io import imread
 
 
 # Data management
@@ -19,56 +22,76 @@ def storeImage(image_file, operation):
         # st.success("Saved File")
         return path
 
+def saveOutput(img, label, path, caption, file_prefix):
+    output_path = ".//static//output//" + label + "//" + path.split("//")[-1]
+    cv2.imwrite(output_path, img)
+    st.image(output_path,  caption=caption)
+    # st.write(output_path)
+    with open(output_path, "rb") as file:
+        btn = st.download_button(
+            label="Download image",
+            data=file,
+            file_name=file_prefix + path.split("//")[-1],
+            mime="image/jpg")
+
+
 # Transformations
 def logTransform(path, c):
-    img = cv2.imread(path)
+    img = cv2.imread(path, flags=cv2.IMREAD_COLOR)
     if c == -1:
         c = 255 / np.log(1 + np.max(img))
     log_image = c * (np.log(img + 1))
     log_image = np.array(log_image, dtype = np.uint8)
-    output_path = ".//static//output//log//" + path.split("//")[-1]
-    cv2.imwrite(output_path, log_image)
-    st.image(output_path,  caption="Logged Image")
-    btn = st.download_button(
-      label="Download image",
-      data=output_path,
-      file_name="log_" + path.split("//")[-1],
-      mime="image/png")
+    saveOutput(log_image, "log", path, "Logged Image", "log_")
 
 def powerTransform(path, c, gamma):
-    img = cv2.imread(path)
+    img = cv2.imread(path, flags=cv2.IMREAD_COLOR)
     if c == -1:
         c = 255 / np.log(1 + np.max(img))
-    log_image = np.array(c*(img/c)**gamma,dtype='uint8')
-    log_image = np.array(log_image, dtype = np.uint8)
-    output_path = ".//static//output//power//" + path.split("//")[-1]
-    cv2.imwrite(output_path, log_image)
-    st.image(output_path,  caption="Powered Image")
-    btn = st.download_button(
-      label="Download image",
-      data=output_path,
-      file_name="log_" + path.split("//")[-1],
-      mime="image/png")
-    
+    power_image = np.array(c*(img/c)**gamma,dtype='uint8')
+    power_image = np.array(power_image, dtype = np.uint8)
+    saveOutput(power_image, "power", path, "Powered Image", "power_")
+
+# For cosine transformation
+# implement 2D DCT
+def dct2(a):
+    return dct(dct(a.T, norm='ortho').T, norm='ortho')
+
+# implement 2D IDCT
+def idct2(a):
+    return idct(idct(a.T, norm='ortho').T, norm='ortho')    
+
+def cosineTransform(path):
+    im = imread(path) 
+    imF = dct2(im)
+    im1 = idct2(imF)
+    im1 = im1
+    saveOutput(im1, "cosine", path, "Cosine Transformed Image", "cosine_")
+
 
 # Translations
 def rotate(path, degree):
-    img = cv2.imread(path)
+    img = cv2.imread(path, flags=cv2.IMREAD_COLOR)
     M = cv2.getRotationMatrix2D((img.shape[1]/2, img.shape[0]/2), degree, 1) 
     rotated_img = cv2.warpAffine(img, M, (img.shape[1], img.shape[0])) 
-    output_path = ".//static//output//rotate//" + path.split("//")[-1]
-    cv2.imwrite(output_path, rotated_img)
-    st.image(output_path,  caption="Rotated Image")
-    btn = st.download_button(
-      label="Download image",
-      data=output_path,
-      file_name="rotated_" + path.split("//")[-1],
-      mime="image/png")
+    saveOutput(rotated_img, "rotate", path, "Rotated Image", "rotated_")
 
+def resize(path, x, y):
+    img = Image.open(path)
+    img = img.resize((x, y), Image.ANTIALIAS)
+    img = np.asarray(img)
+    saveOutput(img, "resize", path, "Resized image", "resized_")
+
+def crop(path, x1, y1, x2, y2):
+    img = Image.open(path)
+    area = (x1, y1, x2, y2)
+    img = img.crop(area)
+    img = np.asarray(img)
+    saveOutput(img, "crop", path, "Cropped Image", "cropped_")
 
 # Adding noise
 def add_saltnpepper(path, x, y):
-    img = cv2.imread(path)
+    img = cv2.imread(path, flags=cv2.IMREAD_COLOR)
     row , col, levels = img.shape
     number_of_pixels = random.randint(x, y)
     for i in range(number_of_pixels):
@@ -80,30 +103,58 @@ def add_saltnpepper(path, x, y):
         y_coord=random.randint(0, row - 1)
         x_coord=random.randint(0, col - 1)
         img[y_coord][x_coord] = 0
-    output_path = ".//static//output//saltnpepper//" + path.split("//")[-1]
-    cv2.imwrite(output_path, img)
-    st.image(output_path,  caption="Noisy Image")
-    btn = st.download_button(
-      label="Download image",
-      data=output_path,
-      file_name="saltnpepper_" + path.split("//")[-1],
-      mime="image/png")
+    saveOutput(img, "saltnpepper", path, "Image with salt and pepper", "saltnpepper_")
+
+def add_gaussian(path):
+    img = skimage.io.imread(path)/255.0
+    gimg = skimage.util.random_noise(img, mode="gaussian")
+    gimg = gimg * 255.0
+    saveOutput(gimg, "gaussian", path, "Image with gaussian noise", "gaussian_")
+
+def add_localvar(path):
+    img = skimage.io.imread(path)/255.0
+    gimg = skimage.util.random_noise(img, mode="localvar")
+    gimg = gimg * 255.0
+    saveOutput(gimg, "localvar", path, "Image with localvar noise", "localvar_")
+
+def add_speckle(path):
+    img = skimage.io.imread(path)/255.0
+    gimg = skimage.util.random_noise(img, mode="speckle")
+    gimg = gimg * 255.0
+    saveOutput(gimg, "speckle", path, "Image with speckle noise", "speckle_")
+
+def add_pepper(path):
+    img = skimage.io.imread(path)/255.0
+    gimg = skimage.util.random_noise(img, mode="pepper")
+    gimg = gimg * 255.0
+    saveOutput(gimg, "pepper", path, "Image with pepper noise", "pepper_")
+
+def add_salt(path):
+    img = skimage.io.imread(path)/255.0
+    gimg = skimage.util.random_noise(img, mode="salt")
+    gimg = gimg * 255.0
+    saveOutput(gimg, "salt", path, "Image with salt noise", "salt_")
+
+def add_poisson(path):
+    img = skimage.io.imread(path)/255.0
+    gimg = skimage.util.random_noise(img, mode="poisson")
+    gimg = gimg * 255.0
+    saveOutput(gimg, "poisson", path, "Image with poisson noise", "poisson_")
+
+def add_sandp(path):
+    img = skimage.io.imread(path)/255.0
+    gimg = skimage.util.random_noise(img, mode="s&p")
+    gimg = gimg * 255.0
+    saveOutput(gimg, "sandp", path, "Image with s&p noise", "sandp_")
 
 def add_boxes(path, num_boxes, size_x, size_y):
-    img = cv2.imread(path)
+    img = cv2.imread(path, flags=cv2.IMREAD_COLOR)
     for i in range(np.random.randint(num_boxes)):
         box_x, box_y = np.random.randint(size_x,size_y, size=2)
         x, y = np.random.randint(0,max(img.shape[0], img.shape[1]) - max(box_x,box_y), size=2)
         img[x:x + box_x, y:y+box_y, :] = 255
-    
-    output_path = ".//static//output//boxes//" + path.split("//")[-1]
-    cv2.imwrite(output_path, img)
-    st.image(output_path,  caption="Boxy Image")
-    btn = st.download_button(
-      label="Download image",
-      data=output_path,
-      file_name="boxy_" + path.split("//")[-1],
-      mime="image/png")
+    saveOutput(img, "boxes", path, "Boxy Image", "boxy_")
+
 
 # Spatial Filtering
 def pixelVal(pix, r1, s1, r2, s2):
@@ -115,20 +166,12 @@ def pixelVal(pix, r1, s1, r2, s2):
         return ((255 - s2)/(255 - r2)) * (pix - r2) + s2
 
 def contrastStretching(path, r1, s1, r2, s2):
-    img = cv2.imread(path)
+    img = cv2.imread(path, flags=cv2.IMREAD_COLOR)
     pixelVal_vec = np.vectorize(pixelVal)
     contrast_stretched = pixelVal_vec(img, r1, s1, r2, s2)
-    output_path = ".//static//output//contrast//" + path.split("//")[-1]
-    cv2.imwrite(output_path, contrast_stretched)
-    st.image(output_path,  caption="Contrasted Image")
-    btn = st.download_button(
-      label="Download image",
-      data=output_path,
-      file_name="contrast_" + path.split("//")[-1],
-      mime="image/png")
+    saveOutput(contrast_stretched, "contrast", path, "Contrast stretched image", "contrast_")
 
 def removeFileCheck(path, thresholdHours = 6):
-    timeNow = datetime.now()
     timestr = datetime.fromtimestamp(os.path.getmtime(path)).strftime('%d,%m,%Y,%H,%M,%S')
     dateFile, monthFile, yearFile, hourFile, minutesFile, secondsFile = map(int, timestr.split(","))
     if (datetime.now().year) > yearFile or datetime.now().month > monthFile or datetime.now().day > dateFile:
@@ -137,9 +180,30 @@ def removeFileCheck(path, thresholdHours = 6):
         return True
     return False
 
-# Other functionalities
-def ImageToText(path):
-    import pytesseract   
-    
+def sharpeningImage(path, level=5):
+    img = cv2.imread(path, flags=cv2.IMREAD_COLOR)
+    kernel = np.array([[0, -1, 0],
+                   [-1, level,-1],
+                   [0, -1, 0]])
+    image_sharp = cv2.filter2D(src=img, ddepth=-1, kernel=kernel)
+    saveOutput(image_sharp, "sharpen", path, "Sharpened Image", "sharpened_")
+
+def blurringImage(path):
+    img = cv2.imread(path, flags=cv2.IMREAD_COLOR)
+    kernel = (1/16.0) * np.array([[1., 2., 1.],
+                                  [2., 4., 2.],
+                                  [1., 2., 1.]])
+    image_sharp = cv2.filter2D(src=img, ddepth=-1, kernel=kernel)
+    saveOutput(image_sharp, "blur", path, "Blurred Image", "blurred_")
+
+def medianFilter(path, size):
+    img = Image.open(path)
+    im2 = np.asarray(img.filter(ImageFilter.MedianFilter(size = size)))
+    saveOutput(im2, "median", path, "Median filtered image", "median_") 
+
+def modeFilter(path, size):
+    img = Image.open(path)
+    im2 = np.asarray(img.filter(ImageFilter.ModeFilter(size = size)))
+    saveOutput(im2, "mode", path, "Mode filtered image", "mode_") 
 
 
